44a07e538b02dfaf78b1ee290d713f6dd3db95fc
Tuyen Le <lethikimtuyen87@gmail.com>
2026-06-18 16:11:15 +0200

    turn conda env names to arguments

--- workflows/jingbao_parallel_eynollah_paddle.sh
+++ workflows/jingbao_parallel_eynollah_paddle.sh
@@ -4,9 +4,15 @@ set -euo pipefail
 WS="${1:-.}"
 
 # Adjust if needed
-ENV_EYNOLLAH="ecpo_eyollah"
-ENV_NON_EYNOLLAH="ecpo_non_eynollah"
+ENV_EYNOLLAH="${2:?Missing env name for Eynollah processes}"
+ENV_NON_EYNOLLAH="${3:?Missing env name for non-Eynollah processes}"
 PROCESSOR_SCRIPT="./individual/jingbao_parallel_individual.sh"
+
+# usage
+if [[ "$WS" == "--help" || "$WS" == "-h" ]]; then
+    echo "Usage: $0 <path_to_workspace> <eynollah_env_name> <non_eynollah_env_name>"
+    exit 0
+fi
 
 echo "Running Eynollah inference step..."
 conda run -n "$ENV_EYNOLLAH" \
